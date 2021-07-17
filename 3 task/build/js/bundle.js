6d70e4868a1955ad5da63a025743af3d87706b70
salenctawer <salenctawer@gmail.com>
2021-07-17 20:55:24 +0500

    first version layout

---
+++ 3 task/build/js/bundle.js
@@ -0,0 +1,5 @@
+/*
+/ /= ../../node_modules/jquery/dist/jquery.js
+ */
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQSIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxyXG4vIC89IC4uLy4uL25vZGVfbW9kdWxlcy9qcXVlcnkvZGlzdC9qcXVlcnkuanNcclxuICovIl19
